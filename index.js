e187edaf4591022fe0414db08637cc1a658c6e85
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2021-06-12 13:39:58 +0800

    sdfsf

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
-
 async function run() {
-  const { sha,repo: {repo, owner} } = github.context;
+  const token = core.getInput('token');
+  const baseBranch = core.getInput('base-branch');
+  const { sha,repo: {repo, owner}, } = github.context;
+  const octokit = github.getOctokit(token);
 
-  // octokit.rest.git.createRef({
-  //   owner: 'kaligo',
-  //   repo: 'e2e-rewards-dashboard',
-  //   ref: 'refs/heads/uat',
-  //   sha: 'westpac-2021-06-07-418f56c5d'
-  // })
-
-  const context = github.context.ref;
-  console.log(sha, repo, owner)
+  const result = await octokit.rest.repos.get({
+    owner: 'kaligo',
+    repo: 'e2e-rewards-dashboard'
+  });
+  console.log(baseBranch)
 }
 
 run();
