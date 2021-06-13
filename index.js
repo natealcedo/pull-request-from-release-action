5b5dd0a5fcc870191f412f02ec53e23eb5ba0f6c
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2021-06-13 15:54:59 +0800

    sdf

--- index.js
+++ index.js
@@ -4,26 +4,35 @@ const github = require('@actions/github');
 async function run() {
   const token = core.getInput('token');
   const baseBranch = core.getInput('base-branch');
-
-  // Get repo information from the context
-  // const { sha, repo: {repo, owner}, tag_name, sha} = github.context;
   const {payload: {repository, release}, sha} = github.context;
-  console.log(github.context);
-  const octokit = github.getOctokit(token);
   const { body, tag_name } = release;
 
+  console.log(JSON.stringify(github.context, null, 2));
+
+
   /**
-   * 1. Get Tag name
-   * 2. Create branch from tag name
-   * 3. Get name of previous release with the associated tag
+   * Since there is no way to know which release this is for, we break down the tag name and get which partner this release is happening
+   * If the parter name is not in the branch, we know this release is not for the associated base-branch
+   */
+  const partner = (tag_name.split('-')[0]).toLowerCase();
+
+  if (!(baseBranch.toLowerCase()).includes(partner)) {
+    process.exit(0);
+  }
+
+  /**
+   * 1. Create branch from tag name
+   * 2. Get name of previous release with the associated tag
    * 3. Create pull request
    */
 
 
+  const octokit = github.getOctokit(token);
   const branchName = `refs/heads/${tag_name}-uat`;
   const title = `${tag_name} UAT release`;
-  const owner = repository.owner;
+  const owner = repository.owner.login;
   const repo = repository.name;
+
 
 
   await octokit.rest.git.createRef({
@@ -38,7 +47,8 @@ async function run() {
     repo,
     title,
     base: baseBranch,
-    body
+    body,
+    head: branchName
   })
 }
 
