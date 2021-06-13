96210343413eb6f4fb6b66bd4a7929a8511f756c
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2021-06-13 15:02:48 +0800

    sdf

--- index.js
+++ index.js
@@ -4,14 +4,37 @@ const github = require('@actions/github');
 async function run() {
   const token = core.getInput('token');
   const baseBranch = core.getInput('base-branch');
-  const { sha,repo: {repo, owner}, } = github.context;
-  const octokit = github.getOctokit(token);
 
-  const result = await octokit.rest.repos.get({
+  // Get repo information from the context
+  // const { sha, repo: {repo, owner}, tag_name, sha} = github.context;
+  const octokit = github.getOctokit(token);
+  // console.log(github.context)
+
+  /**
+   * 1. Get Tag name
+   * 2. Create branch from tag name
+   * 3. Get name of previous release with the associated tag
+   * 3. Create pull request
+   */
+  const release = await octokit.rest.repos.getReleaseByTag({
     owner: 'kaligo',
-    repo: 'e2e-rewards-dashboard'
-  });
-  console.log(baseBranch)
+    repo: 'e2e-rewards-dashboard',
+    tag: 'fab-2021-06-07-418f56c5d'
+  })
+
+  console.log(release)
+
+  // const branchName = `refs/heads/${tag_name}-uat`;
+
+
+  // await octokit.rest.git.createRef({
+  //   owner,
+  //   repo,
+  //   sha,
+  //   ref: branchName
+  // });
+
+  // octokit.rest.git.cre
 }
 
 run();
