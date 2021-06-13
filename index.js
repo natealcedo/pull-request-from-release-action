8ba1f943dc208d5612f11f89d6772856e7d3866d
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2021-06-13 15:46:05 +0800

    blah

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ async function run() {
 
   // Get repo information from the context
   // const { sha, repo: {repo, owner}, tag_name, sha} = github.context;
+  const {payload: {repository, release}, sha} = github.context;
   console.log(github.context);
   const octokit = github.getOctokit(token);
+  const { body, tag_name } = release;
 
   /**
    * 1. Get Tag name
@@ -16,34 +18,28 @@ async function run() {
    * 3. Get name of previous release with the associated tag
    * 3. Create pull request
    */
-  // const release = await octokit.rest.repos.getReleaseByTag({
-  //   owner,
-  //   repo,
-  //   tag: tag_name
-  // })
-
-  // const prBody = release.data.body;
-
-  // const branchName = `refs/heads/${tag_name}-uat`;
-  // const title = `${tag_name} UAT release`;
 
 
-  // await octokit.rest.git.createRef({
-  //   owner,
-  //   repo,
-  //   sha,
-  //   ref: branchName
-  // });
+  const branchName = `refs/heads/${tag_name}-uat`;
+  const title = `${tag_name} UAT release`;
+  const owner = repository.owner;
+  const repo = repository.name;
 
-  // await octokit.rest.pulls.create({
-  //   owner,
-  //   repo,
-  //   title,
-  //   base: baseBranch,
-  //   body: prBody
-  // })
 
-  // console.log('done');
+  await octokit.rest.git.createRef({
+    owner,
+    repo,
+    sha,
+    ref: branchName
+  });
+
+  await octokit.rest.pulls.create({
+    owner,
+    repo,
+    title,
+    base: baseBranch,
+    body
+  })
 }
 
 run();
